523ecd07d0f3f8a4f64b131f87e4353584e46f19
Coco <cm_heroku-543@not.allaboutcoco.com>
2015-05-05 06:52:14 -0700

    fixup! I2: uses an ERB template and HTML

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,21 @@ require 'sinatra/reloader'
 
 get '/' do
   prng = Random.new
-  number = prng.rand(100)
-  erb :index, :locals => {:number => number}
+  SECRET_NUMBER = prng.rand(3)
+  str_guess =  params["guess"]
+  message = check_guess(str_guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+end
+
+def check_guess(str_guess)
+  if !str_guess.nil? && !str_guess.empty?
+    i_guess = str_guess.to_i
+    if i_guess > SECRET_NUMBER
+      "Too high!"
+    elsif i_guess == SECRET_NUMBER
+      "You got it right!"
+    else
+      "Too low!"
+    end
+  end
 end
