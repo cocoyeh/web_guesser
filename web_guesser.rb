f0dd785431a9603bb30723bfa62b7006dabc64ee
Coco <cm_heroku-543@not.allaboutcoco.com>
2015-05-08 13:26:24 -0700

    I3: Guessing Numbers

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,17 +6,21 @@ get '/' do
   SECRET_NUMBER = prng.rand(3)
   str_guess =  params["guess"]
   message = check_guess(str_guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  color = "green"
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
 end
 
 def check_guess(str_guess)
   if !str_guess.nil? && !str_guess.empty?
     i_guess = str_guess.to_i
     if i_guess > SECRET_NUMBER
+      color = "red"
       "Too high!"
     elsif i_guess == SECRET_NUMBER
+      color = "green"
       "You got it right!"
     else
+      color = "blue"
       "Too low!"
     end
   end
